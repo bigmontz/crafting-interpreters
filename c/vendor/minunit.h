0d37e890698ab568e474e022b97a7b5f9c20fd8b
Antonio Barcelos <antonio.barcelos@neo4j.com>
2022-10-04 18:52:31 +0200

    c: Do not stop in the first failed test

--- c/vendor/minunit.h
+++ c/vendor/minunit.h
@@ -6,37 +6,36 @@
     if (!(test))                 \
       return message;            \
   } while (0)
-#define mu_run_test(description, test) \
-  do                                   \
-  {                                    \
-    printf("\t");                      \
-    printf(description);               \
-    printf("...");                     \
-    char *message = test();            \
-    tests_run++;                       \
-    if (message)                       \
-    {                                  \
-      puts("\033[0;31mFAIL\033[0m");   \
-      printf("\t\t Reason: ");         \
-      puts(message);                   \
-      return message;                  \
-    }                                  \
-    puts(" \033[0;32mOK\033[0m");      \
+#define mu_run_test(description, test)                \
+  do                                                  \
+  {                                                   \
+    printf("\t");                                     \
+    printf(description);                              \
+    printf("...");                                    \
+    char *message = test();                           \
+    tests_run++;                                      \
+    if (message)                                      \
+    {                                                 \
+      tests_failed++;                                 \
+      printf("\033[0;31mFAIL: %s\033[0m\n", message); \
+    }                                                 \
+    else                                              \
+    {                                                 \
+      puts(" \033[0;32mOK\033[0m");                   \
+    }                                                 \
   } while (0);
 
 extern int tests_run;
+extern int tests_failed;
 
-#define mu_test_suite(name, run_tests)    \
-  int tests_run = 0;                      \
-  int main(int argc, char **argv)         \
-  {                                       \
-    printf("Running ");                   \
-    puts(name);                           \
-    char *result = run_tests();           \
-    if (result == 0)                      \
-    {                                     \
-      printf("ALL TESTS PASSED\n");       \
-    }                                     \
-    printf("Tests run: %d\n", tests_run); \
-    return result != 0;                   \
+#define mu_test_suite(name, run_tests)                                \
+  int tests_run = 0;                                                  \
+  int tests_failed = 0;                                               \
+  int main(int argc, char **argv)                                     \
+  {                                                                   \
+    printf("Running ");                                               \
+    puts(name);                                                       \
+    char *result = run_tests();                                       \
+    printf("Tests run: %d, Failures: %d\n", tests_run, tests_failed); \
+    return tests_failed != 0;                                         \
   }
