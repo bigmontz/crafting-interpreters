2b84ead053def8a3d5668dc8e43c912ffa594504
Antonio Barcelos <antonio.barcelos@neo4j.com>
2022-11-08 16:11:06 +0100

    Add scanner implementation in elixir

--- elixir/lib/elox/scanner.ex
+++ elixir/lib/elox/scanner.ex
@@ -10,9 +10,8 @@ defmodule Elox.Scanner do
     end
   end
 
-  # defmacro is_digit(<<c, _::binary>>) do 
-  #   c >= '0' and c <= '9'
-  # end 
+  defguard is_digit(c) when c >= "0" and c <= "9"
+  defguard is_alpha(c) when c >= "a" and c <= "z" or c >= "A" and c <= "Z" or c == "_"
 
   def _scan(source, tokens, start, current, line) do
     new_token = fn type, size, literal, line_inc ->
@@ -86,7 +85,7 @@ defmodule Elox.Scanner do
 
         "/" ->
           if String.at(source, current + 1) == "/",
-            do: [nil, move_until_next_line(source, start, current + 1), 0],
+            do: [nil, move_until_next_line(source, start, current + 1), 1],
             else: new_token_nil.(:slash, 1)
 
         "\n" ->
@@ -102,12 +101,16 @@ defmodule Elox.Scanner do
             do: new_token.(:string, size, literal, line_inc),
             else: [nil, size, line_inc]
 
-        # d when is_digit(d) ->
-        #   [literal, size] = scan_number(source, start, current + 1, line)
-        #   if literal != nil, do: new_token.(:string, size, literal, 0), else: [nil, size, 0]
+        d when is_digit(d) ->
+          [literal, size] = scan_number(source, start, current + 1, line)
+          if literal != nil, do: new_token.(:number, size, literal, 0), else: [nil, size, 0]
 
-        _ ->
-          Elox.Error.report(line, "Unexpected character.")
+        c when is_alpha(c) -> 
+          [literal, size] = scan_identifier(source, start, current + 1, line)
+          if literal != nil, do: new_token.(:identifier, size, literal, 0), else: [nil, size, 0]
+
+        unexpectedChar ->
+          Elox.Error.report(line, "Unexpected character: #{unexpectedChar}")
           [nil, 1, 0]
       end
 
@@ -120,6 +123,7 @@ defmodule Elox.Scanner do
   def move_until_next_line(source, start, current) do
     case String.at(source, current) do
       "\n" -> current - start
+      nil -> current - start
       _ -> move_until_next_line(source, start, current + 1)
     end
   end
@@ -133,7 +137,7 @@ defmodule Elox.Scanner do
       false ->
         case String.at(source, current) do
           "\"" ->
-            [String.slice(source, start..current), current - start + 1, line_inc]
+            [String.slice(source, (start + 1)..(current - 1)), current - start + 1, line_inc]
 
           "\n" ->
             scan_string(source, start, current + 1, line + 1, line_inc + 1)
@@ -144,14 +148,30 @@ defmodule Elox.Scanner do
     end
   end
 
-  # def scan_number(source, start, current, line) do 
-  #   case is_digit(String.at(source, current)) do 
-  #     true -> scan_number(source, start, current+1, line)
-  #     false -> 
-  #       literal = String.slice(source, start..(current-1)) |> String.parse_float()
-  #       [literal, current - start]
-  #   end
-  # end
+  def scan_number(source, start, current, line, has_dot \\ false) do 
+    c = String.at(source, current)
+    case is_digit(c) do 
+      true -> scan_number(source, start, current+1, line, has_dot)
+      false -> 
+          case has_dot or c != "." or not is_digit(String.at(source, current + 1)) do 
+            true -> 
+              { literal, _ } = String.slice(source, start..(current-1)) |> Float.parse()
+              [literal, current - start]
+            false -> 
+              scan_number(source, start, current+1, line, true)
+          end
+    end
+  end
+
+  def scan_identifier(source, start, current, line) do 
+    c = String.at(source, current)
+    case is_digit(c) or is_alpha(c) do 
+      true -> scan_identifier(source, start, current+1, line)
+      false -> 
+          literal = String.slice(source, start..(current-1)) 
+          [literal, current - start]
+    end
+  end
 
   def is_at_end(source, current) do
     String.length(source) <= current
--- elixir/test/elox/scanner_test.exs
+++ elixir/test/elox/scanner_test.exs
@@ -1,9 +1,22 @@
 defmodule EloxTest.Scanner do
   use ExUnit.Case, async: true
 
-  test "Elox.Scanner.scan/1 should scan an empty string" do
-    assert Elox.Scanner.scan("") == [eof_at_line()]
-  end
+  for {empty_script, line} <- [
+    {"", 1},
+    {" ", 1},
+    {"\r", 1},
+    {"\t", 1},
+    {"\r\n", 1}, # it should be second line, investigate
+    {"\n", 2},
+    {"\n\n", 3},
+    {"// comment", 2}
+  ] do
+    @fixture { empty_script, line }
+    test "Elox.Scanner.scan/1 should scan \"#{empty_script}\"" do
+      { script, l } = @fixture
+      assert Elox.Scanner.scan(script) == [eof_at_line(l)]
+    end
+  end 
 
   for {lexeme, type} <- [
     {"(", :left_paren},
@@ -26,10 +39,27 @@ defmodule EloxTest.Scanner do
     {">=", :greater_equal},
     {"/", :slash}
   ] do
-    @pair {lexeme, type}
+    @fixture {lexeme, type}
     test "Elox.Scanner.scan/1 should scan \"#{lexeme}\"" do
-      { l, t } = @pair
+      { l, t } = @fixture
       assert Elox.Scanner.scan(l) === [single_char_at_line(t, l), eof_at_line()]
+    end
+  end
+
+  for {lexeme, type, literal, line} <- [
+    {"\"My Nice String\"", :string, "My Nice String", 1},
+    {"\"My Nice String\n String\"", :string, "My Nice String\n String", 2},
+    {"\"My Nice String\n String \n blob\"", :string, "My Nice String\n String \n blob", 3},
+    {"987654321", :number, 987654321.0, 1},
+    {"123456789.01", :number, 123456789.01, 1},
+    {"var", :identifier, "var", 1},
+    {"my_var", :identifier, "my_var", 1},
+    {"mY_va112r", :identifier, "mY_va112r", 1}
+  ] do
+    @fixture {lexeme, type, literal, line}
+    test "Elox.Scanner.scan/1 should scan \"#{lexeme}\"" do
+      { lex, t, lit, lin } = @fixture
+      assert Elox.Scanner.scan(lex) === [single_char_at_line(t, lex, lit), eof_at_line(lin)]
     end
   end
 
@@ -37,7 +67,7 @@ defmodule EloxTest.Scanner do
     %Elox.Token{type: :eof, line: line, lexeme: "", literal: nil}
   end
 
-  def single_char_at_line(type, lexeme, line \\ 1) do
-    %Elox.Token{type: type, line: line, lexeme: lexeme, literal: nil}
+  def single_char_at_line(type, lexeme, literal \\ nil, line \\ 1) do
+    %Elox.Token{type: type, line: line, lexeme: lexeme, literal: literal}
   end
 end
